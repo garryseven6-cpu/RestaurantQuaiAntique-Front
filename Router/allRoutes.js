ddc5fbb51ccff38a472778dd8268b8736bf0ff38
Duchesse <bouboulelover23@gmail.com>
2025-11-14 15:17:36 +0100

    Ajout page d'accueil

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/Home.html"),
+    new Route("/Galerie", "La galerie", "/pages/Galerie.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
