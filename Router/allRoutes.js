4e60599773616edc5ae4165a1b8a27b5f4553054
Duchesse <bouboulelover23@gmail.com>
2025-11-14 18:35:57 +0100

    Ajouts pages Signin/Signup/Account/EditPassword

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/Home.html"),
     new Route("/Galerie", "La galerie", "/pages/Galerie.html"),
     new Route("/Signin", "Connexion", "/pages/Signin.html"),
+    new Route("/Signup", "Inscripton", "/pages/Signup.html"),
+    new Route("/Account", "Mon compte", "/pages/Account.html"),
+    new Route("/EditPassword", "Changer mon mot de passe", "/pages/EditPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
