805320ecac2fd7e4e08dd26c13935593c9e85513
Duchesse <bouboulelover23@gmail.com>
2025-11-25 13:52:34 +0100

    Ajouts pages de réservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,9 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/Home.html"),
     new Route("/Galerie", "La galerie", "/pages/Galerie.html"),
     new Route("/Signin", "Connexion", "/pages/Signin.html"),
-    new Route("/Signup", "Inscripton", "/pages/Signup.html"),
+    new Route("/Signup", "Inscripton", "/pages/Signup.html", "/js/Auth/Signup.js"),
     new Route("/Account", "Mon compte", "/pages/Account.html"),
     new Route("/EditPassword", "Changer mon mot de passe", "/pages/EditPassword.html"),
+    new Route("/AllResa", "Vos réservations", "/pages/Reservations/AllResa.html"),
+    new Route("/Reserver", "réservation", "/pages/Reservations/Reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
